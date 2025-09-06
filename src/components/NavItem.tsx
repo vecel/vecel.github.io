ef1503543308efd777ca10de659d98f508038375
vecel <mateusz.karandys@gmail.com>
2025-09-06 11:21:53 +0200

    Add drawer

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,20 +1,18 @@
 import type { ComponentType } from "react"
 import type { SvgIconProps } from "@mui/material/SvgIcon"
 
-
 interface NavItemProps {
     name: string,
     icon: ComponentType<SvgIconProps>,
-    expanded: boolean,
-    active: boolean
+    active: boolean,
 }
 
-export default function NavItem({ name, icon: IconComponent, expanded, active }: NavItemProps) {
+export default function NavItem({ name, icon: IconComponent, active }: NavItemProps) {
+    
     return (
-        <li className="w-full h-12 flex items-center justify-center cursor-pointer"
-            style={{ color: active ? "red" : "black"}}>
+        <li className="flex flex-col items-center p-2 m-2 text-sm rounded-2xl hover:bg-secondary-fixed hover:text-on-secondary-container cursor-pointer">
             <IconComponent />
-            { expanded && name}
+            {name}
         </li>
     )
 }
